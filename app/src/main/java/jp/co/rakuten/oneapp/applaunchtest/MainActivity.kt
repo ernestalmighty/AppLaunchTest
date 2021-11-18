33517b88b627429f88f0d4037967b981a7dcad69
Ernest Martin Gayyed <ernestgayyed@gmail.com>
2021-11-18 13:08:02 +0800

    initial commit

---
+++ app/src/main/java/jp/co/rakuten/oneapp/applaunchtest/MainActivity.kt
@@ -0,0 +1,12 @@
+package jp.co.rakuten.oneapp.applaunchtest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import jp.co.rakuten.oneapp.applaunchtest.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ macrobenchmark/src/main/java/jp/co/rakuten/oneapp/macrobenchmark/SampleStartupBenchmark.kt
@@ -0,0 +1,37 @@
+package jp.co.rakuten.oneapp.macrobenchmark
+
+import android.content.Intent
+import androidx.benchmark.macro.StartupMode
+import androidx.benchmark.macro.StartupTimingMetric
+import androidx.benchmark.macro.junit4.MacrobenchmarkRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class SampleStartupBenchmark {
+    @get:Rule
+    val benchmarkRule = MacrobenchmarkRule()
+
+    @Test
+    fun startup() = benchmarkRule.measureStartup(
+        profileCompiled = false,
+        startupMode = StartupMode.COLD,
+        iterations = 3
+    ) {
+        action = "jp.co.rakuten.oneapp.main.MAIN_ACTIVITY"
+    }
+
+    companion object {
+        @Parameterized.Parameters(name = "mode={0}")
+        @JvmStatic
+        fun parameters(): List<Array<Any>> {
+            return listOf(StartupMode.COLD, StartupMode.WARM, StartupMode.HOT)
+                .map { arrayOf(it) }
+        }
+    }
+}
